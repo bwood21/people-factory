e6365fc9f46aed91d7536618d28286b7d0d6db60
Brandon Wood <wood149@purdue.edu>
2017-05-15 11:32:16 -0400

    Add stylesheets

--- index.js
+++ index.js
@@ -6,10 +6,16 @@ personForm.onsubmit = (ev) => {
     const details = document.querySelector('.details')
 
     const personName = (form.personName.value)
+    //form.elements gives array
+
     //details.textContent = personName
     // inner html allows taggs
-    details.innerHTML = '<em>' + personName + '</em>'
-    //form.elements gives array
+    //details.innerHTML = '<em>' + personName + '</em>'
+   // details.innerHTML = `<em>${personName}</em>`
+    
+    const em = document.createElement('em')
+    em.textContent = personName;
+    details.appendChild(em);
     //console.log(ev)
     //debugger
 }
